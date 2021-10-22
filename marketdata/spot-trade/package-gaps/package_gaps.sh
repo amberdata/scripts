9f70e33927d1a8130093d1f75e669cf9f32102f2
Shar <shar@amberdata.io>
2021-10-22 14:08:47 -0400

    updated

--- marketdata/spot-trade/package-gaps/package_gaps.sh
+++ marketdata/spot-trade/package-gaps/package_gaps.sh
@@ -2,26 +2,59 @@
 
 ####################################################################################
 
-DATA_DIR="./data"
-OUTPUT_DIR="./output"
+DATA_DIR=$(echo "$(pwd)/data")
+rm -rf "${DATA_DIR}"
+
+OUTPUT_DIR=$(echo "$(pwd)/output")
+OUT="_OUT"
+ERR="_ERR"
+rm -f $OUT && rm -f $ERR
 
 ####################################################################################
 
-aws s3 ls s3://amberdata-marketdata/spot-trade/ | sed -e 's# *PRE \([^/]*\)/#\1#' | sort | while read -r DATE
+aws s3 ls s3://amberdata-marketdata/spot-trade/ | sed -e 's# *PRE \([^/]*\)/#\1#' | sort -n -r | while read -r DATE
 do
+
+  mkdir -p "${DATA_DIR}"
+
   echo "DATE: $DATE"
 
   DATE_PATH="s3://amberdata-marketdata/spot-trade/$DATE/"
 
-  aws s3 cp "${DATE_PATH}" data/ --recursive --exclude "*" --include "*_GAPS"
+  echo " DOWNLOADING FILES FOR $DATE"
+
+  start=`date +%s.%N`
+
+  aws s3 cp "${DATE_PATH}" "${DATA_DIR}" --recursive --exclude "*" --include "*_GAPS" >> "${OUT}" 2>> "${ERR}"
+
+  end=`date +%s.%N`
+
+  runtime=$( echo "$end - $start" | bc -l )
+
+  echo " Runtime: ${runtime}"; echo "";
+
+  echo " MOVING FILES FOR DATE $DATE"
+
+  start=`date +%s.%N`
 
   ls $DATA_DIR | while read -r PAIR
   do
-    DATA_FILE_PATH="${DATA_DIR}/${PAIR}/_GAPS"
-    PAIR_DIR = "${OUTPUT_DIR}/${PAIR}"
-    mkdir -p PAIR_DIR
-    OUTPUT_FILE_PATH="${PAIR_DIR}/${DATE}._GAPS"
-    mv "${DATA_FILE_PATH}"  "${OUTPUT_FILE_PATH}"
+
+    SOURCE_GAP_FILE_PATH="${DATA_DIR}/${PAIR}/_GAPS"
+    DEST="${OUTPUT_DIR}/${PAIR}"
+
+    mkdir -p "${DEST}"
+    
+    DEST_GAP_FILE_PATH="${DEST}/${DATE}._GAPS"
+    mv "${SOURCE_GAP_FILE_PATH}" "${DEST_GAP_FILE_PATH}"
   done
+
+  end=`date +%s.%N`
+
+  runtime=$( echo "$end - $start" | bc -l )
+
+  echo " Runtime: ${runtime}"; echo "";
+
+  rm -rf "${DATA_DIR}"
 
 done
