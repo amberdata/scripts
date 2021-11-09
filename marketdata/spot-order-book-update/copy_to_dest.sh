2765f5df7c96c9a978d9137ac6bf53d3c787f435
Shahriyar Pruisken <spruisken@gmail.com>
2021-11-09 10:58:00 -0500

    added spot-order-book-update copy scripts

---
+++ marketdata/spot-order-book-update/copy_to_dest.sh
@@ -0,0 +1,23 @@
+
+#!/bin/bash
+
+####################################################################################
+
+START_DATE=$1
+END_DATE=$2
+
+SOURCE="s3://amberdata-marketdata/spot-order-book-update"
+DEST="s3://amberdata-repository/rentec/spot-order-book-update"
+
+####################################################################################
+
+DATES=( $(
+  aws s3 ls "${SOURCE}/" | awk '{ print $2 }' | sed 's/.$//' | sort -n -r | while read -r dt
+  do
+    [[ ! "${dt}" < "${START_DATE}" ]] && [[ ! "${dt}" > "${END_DATE}" ]] && echo "${dt}"
+  done
+) )
+
+parallel ./sync_dt {} $SOURCE $DEST ::: ${DATES[@]}
+
+####################################################################################
