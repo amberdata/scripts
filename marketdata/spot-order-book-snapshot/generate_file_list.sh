e16a8561e7d9d627cbc10915dbeced3398c08f54
Shahriyar Pruisken <spruisken@gmail.com>
2021-11-09 18:47:26 -0500

    fix

--- marketdata/spot-order-book-snapshot/generate_file_list.sh
+++ marketdata/spot-order-book-snapshot/generate_file_list.sh
@@ -6,10 +6,34 @@
 START_DATE=$1
 END_DATE=$2
 
-aws s3 cp s3://amberdata-marketdata/order_book_snapshot/ . --recursive --dryrun > s3.out 2> s3.err
+OUT=s3.out
+ERR=s3.err
 
-time sed -e 's/.* \(s3.*\) to .*/\1/' s3.out > s3.out.files
+FILES=s3.out.files
 
+CANDIDATES=candidates
+
+# time aws s3 cp s3://amberdata-marketdata/order_book_snapshot/ . --recursive --dryrun > $OUT 2> $ERR
+
+time sed -e 's/.* \(s3.*\) to .*/\1/' $OUT > $FILES
+
+while IFS="" read -r LINE || [ -n "$LINE" ]
+do
+  DATE=$(echo $LINE | grep -Eo '[[:digit:]]{4}-[[:digit:]]{2}-[[:digit:]]{2}')
+
+  if [[ "${DATE}" < "${START_DATE}" ]];
+  then
+    continue
+  fi
+
+  if [[ "${DATE}" > "${END_DATE}" ]];
+  then
+    continue
+  fi
+
+  echo $LINE >> CANDIDATES
+
+done < $FILES
 
 
 ####################################################################################
