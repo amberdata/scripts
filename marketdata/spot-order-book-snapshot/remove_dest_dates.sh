53df438655781774718214073553fd381127c964
Shahriyar Pruisken <spruisken@gmail.com>
2021-11-12 13:45:23 -0500

    add scripts

---
+++ marketdata/spot-order-book-snapshot/remove_dest_dates.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+####################################################################################
+
+START_DATE=$1
+END_DATE=$2
+
+DEST="s3://amberdata-repository/rentec/spot-order-book-snapshots"
+
+####################################################################################
+
+DATES=( $(
+  aws s3 ls "${DEST}/" | awk '{ print $2 }' | sed 's/.$//' | sort -n -r | while read -r dt
+  do
+    [[ ! "${dt}" < "${START_DATE}" ]] && [[ ! "${dt}" > "${END_DATE}" ]] && echo "${dt}"
+  done
+) )
+
+parallel ./remove_dest_date.sh $DEST {} ::: ${DATES[@]}
+
+####################################################################################
